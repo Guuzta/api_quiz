18add9cb82a3ddb7842ec3f4871cb39086f1bb94
Guuzta <Guuhsilva13@outlook.com>
2025-10-04 13:35:41 -0300

    criando schema de usuario

---
+++ src/models/users.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('users', userSchema)
+
+export default User
