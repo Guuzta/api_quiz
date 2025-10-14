561cb9d22e69b6e2a5139eb518efc56017df50bd
Guuzta <Guuhsilva13@outlook.com>
2025-10-14 17:03:25 -0300

    criando schema de quiz

---
+++ src/models/quiz.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const quizSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    difficulty: {
+        type: String,
+        enum: ['fácil', 'médio', 'difícil'],
+        required: true,
+        lowercase: true
+    },
+    questions: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Question',
+        required: true
+    }],
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { 
+    timestamps: true 
+})
+
+const Quiz = mongoose.model('Quiz', quizSchema)
+
+export default Quiz
