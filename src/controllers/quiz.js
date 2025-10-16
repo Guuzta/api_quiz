992d093a1aa3e6b9f58a03ab0858b0f1222d2c18
THINKPAD T490 <Guuhsilva13@outlook.com>
2025-10-16 19:59:11 -0300

    criando rota para buscar quiz pelo ID

--- src/controllers/quiz.js
+++ src/controllers/quiz.js
@@ -58,6 +58,44 @@ const createQuiz = async (req, res) => {
     }
 }
 
+const getQuizById = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const isValid = mongoose.Types.ObjectId.isValid(id)
+
+        if(!isValid) {
+            return res.status(400).json({
+                success: false,
+                message: 'ID do Quiz inválido!'
+            })
+        }
+
+        const quiz = await Quiz.findById(id).populate('questions')
+
+        if(!quiz) {
+            return res.status(404).json({
+                success: false,
+                message: 'Quiz não encontrado!'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Quiz encontrado com sucesso!',
+            quiz
+        })
+    } catch (error) {
+        console.log('Erro interno no servidor ao buscar Quiz!', error)
+
+        res.status(500).json({
+            success: false,
+            message: 'Erro interno no servidor ao buscar Quiz!'
+        })
+    }
+}
+
 export {
-    createQuiz
+    createQuiz,
+    getQuizById
 }
--- src/routes/quizzes.js
+++ src/routes/quizzes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 
 import {
-    createQuiz
+    createQuiz,
+    getQuizById
 } from '../controllers/quiz.js'
 
 import authenticateToken from '../middleware/authenticateToken.js'
@@ -16,6 +17,12 @@ router.post(
     authenticateToken,
     validateRequest(createQuizSchema),
     createQuiz
+)
+
+router.get(
+    '/quizzes/:id',
+    authenticateToken,
+    getQuizById
 )
 
 export default router
