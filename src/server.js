2c155a145bc1bd5c09c52fc7d87d831095c3c3a7
Guuzta <Guuhsilva13@outlook.com>
2025-11-09 14:48:31 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 
 import connectDB from './config/database.js'
 
@@ -17,9 +18,10 @@ connectDB()
 startCronJobs()
 
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
 app.use(helmet())
+app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/admin', adminRoutes)
