0fbc5c30c52ea13e96543a3f7499de2c9aba909a
Guuzta <Guuhsilva13@outlook.com>
2025-11-14 17:38:18 -0300

    documentando rotas auth

--- src/validations/register.js
+++ src/validations/register.js
@@ -2,10 +2,10 @@ import { object, string} from 'yup'
 
 const registerUserSchema = object({
     name: string()
-        .required('Campo nome é obrigatório')
+        .required('Campo nome é obrigatório!')
         .min(4, 'O nome precisa ter pelo menos 4 caracteres!'),
     email: string()
-        .required('Campo email é obrigatório')
+        .required('Campo email é obrigatório!')
         .email('Formato de email inválido!'),
     password: string()
         .required('Formato de senha inválido!')
